b39e07d790d47b7d913949ede78f752a74c6c370
Ashish <157567921+Ashishjaglan01@users.noreply.github.com>
2025-12-16 23:10:04 +0530

    Delete Backend/Backend directory

--- Backend/Backend/database/db.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-from config import MONGO_URI, DB_NAME
-
-client = MongoClient(MONGO_URI)
-db = client[DB_NAME]
-
-chats_collection = db["chats"]
-quiz_collection = db["quizzes"]
